7009997e1550d12644610719fb7d2e216feaba05
xufei <xu.fei@outlook.com>
2015-09-08 11:26:29 +0800

    改了一下demo

--- src/modules/demo/controllers/calendar.js
+++ src/modules/demo/controllers/calendar.js
@@ -1,8 +1,10 @@
 export default class CalendarController {
 	constructor($scope) {
-		$scope.now = new Date(2011, 2, 3);
-		$scope.min = new Date(2015, 8, 3);
+		$scope.now = new Date(2012, 3, 9);
+		$scope.min = new Date(2015, 8, 2);
 		$scope.max = new Date(2016, 2, 1);
+
+		$scope.arr = Array.from(Array(3), (v, k) => Array.from(Array(4), (v1, k1) => new Date(2015, k * 4 + k1, 1)));
 	}
 }
 
